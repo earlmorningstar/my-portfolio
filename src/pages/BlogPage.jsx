e554147d32788e75cfd2f69b48f465c6ee21b43d
Onyeabor Joel <earlmorningstar@gmail.com>
2024-10-16 17:28:37 +0100

    Commit III.

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,8 +1,169 @@
+import ConnectionNote from "./ConnectionNote";
+import "./Index.css";
+import { MdOutlineReadMore } from "react-icons/md";
+
 function BlogPage() {
-    return(
-        <h1>I am BlogPage</h1>
-    )
+  const dispatchItems = Array(12).fill({
+    image: "/images/silhouette.jfif",
+    text: "This is an inspirational quote for you to improve yourself and get better. Fight until the end.",
+  });
+
+  return (
+    <>
+      <div className="blogpage-main-container">
+        <h3>Intel Briefings</h3>
+        <div className="latest-dispatch-container">
+          {dispatchItems.map((item, index) => (
+            <div className="dispatch-image-container" key={index}>
+              <img src={item.image} alt="Profile" />
+              <p className="dispatch-overlay-text">
+                {item.text}
+                <p>
+                  Dig Deeper <MdOutlineReadMore size={25} />
+                </p>
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <ConnectionNote />
+    </>
+  );
 }
 
-
 export default BlogPage;
+
+// import "./Index.css";
+// import { MdOutlineReadMore } from "react-icons/md";
+
+// function BlogPage() {
+//   return (
+//     <div className="page-main-container">
+//       <h3>Intel Briefings</h3>
+//       <div className="latest-dispatch-container">
+//         <div className="dispatch-image-container">
+//           <img src="/images/silhouette.jfif" alt="Profile" />
+//           <p className="dispatch-overlay-text">
+//             This is an inspirational quote for you to improve yourself and get
+//             better. Fight until the end.{" "}
+//             <p>
+//               Dig Deeper <MdOutlineReadMore size={25} />
+//             </p>
+//           </p>
+//         </div>
+//         <div className="dispatch-image-container">
+//           <img src="/images/silhouette.jfif" alt="Profile" />
+//           <p className="dispatch-overlay-text">
+//             This is an inspirational quote for you to improve yourself and get
+//             better. Fight until the end.{" "}
+//             <p>
+//               Dig Deeper <MdOutlineReadMore size={25} />
+//             </p>
+//           </p>
+//         </div>
+//         <div className="dispatch-image-container">
+//           <img src="/images/silhouette.jfif" alt="Profile" />
+//           <p className="dispatch-overlay-text">
+//             This is an inspirational quote for you to improve yourself and get
+//             better. Fight until the end.{" "}
+//             <p>
+//               Dig Deeper <MdOutlineReadMore size={25} />
+//             </p>
+//           </p>
+//         </div>
+//         <div className="dispatch-image-container">
+//           <img src="/images/silhouette.jfif" alt="Profile" />
+//           <p className="dispatch-overlay-text">
+//             This is an inspirational quote for you to improve yourself and get
+//             better. Fight until the end.{" "}
+//             <p>
+//               Dig Deeper <MdOutlineReadMore size={25} />
+//             </p>
+//           </p>
+//         </div>
+//         <div className="dispatch-image-container">
+//           <img src="/images/silhouette.jfif" alt="Profile" />
+//           <p className="dispatch-overlay-text">
+//             This is an inspirational quote for you to improve yourself and get
+//             better. Fight until the end.{" "}
+//             <p>
+//               Dig Deeper <MdOutlineReadMore size={25} />
+//             </p>
+//           </p>
+//         </div>
+//         <div className="dispatch-image-container">
+//           <img src="/images/silhouette.jfif" alt="Profile" />
+//           <p className="dispatch-overlay-text">
+//             This is an inspirational quote for you to improve yourself and get
+//             better. Fight until the end.{" "}
+//             <p>
+//               Dig Deeper <MdOutlineReadMore size={25} />
+//             </p>
+//           </p>
+//         </div>
+//         <div className="dispatch-image-container">
+//           <img src="/images/silhouette.jfif" alt="Profile" />
+//           <p className="dispatch-overlay-text">
+//             This is an inspirational quote for you to improve yourself and get
+//             better. Fight until the end.{" "}
+//             <p>
+//               Dig Deeper <MdOutlineReadMore size={25} />
+//             </p>
+//           </p>
+//         </div>
+//         <div className="dispatch-image-container">
+//           <img src="/images/silhouette.jfif" alt="Profile" />
+//           <p className="dispatch-overlay-text">
+//             This is an inspirational quote for you to improve yourself and get
+//             better. Fight until the end.{" "}
+//             <p>
+//               Dig Deeper <MdOutlineReadMore size={25} />
+//             </p>
+//           </p>
+//         </div>
+//         <div className="dispatch-image-container">
+//           <img src="/images/silhouette.jfif" alt="Profile" />
+//           <p className="dispatch-overlay-text">
+//             This is an inspirational quote for you to improve yourself and get
+//             better. Fight until the end.{" "}
+//             <p>
+//               Dig Deeper <MdOutlineReadMore size={25} />
+//             </p>
+//           </p>
+//         </div>
+//         <div className="dispatch-image-container">
+//           <img src="/images/silhouette.jfif" alt="Profile" />
+//           <p className="dispatch-overlay-text">
+//             This is an inspirational quote for you to improve yourself and get
+//             better. Fight until the end.{" "}
+//             <p>
+//               Dig Deeper <MdOutlineReadMore size={25} />
+//             </p>
+//           </p>
+//         </div>
+//         <div className="dispatch-image-container">
+//           <img src="/images/silhouette.jfif" alt="Profile" />
+//           <p className="dispatch-overlay-text">
+//             This is an inspirational quote for you to improve yourself and get
+//             better. Fight until the end.{" "}
+//             <p>
+//               Dig Deeper <MdOutlineReadMore size={25} />
+//             </p>
+//           </p>
+//         </div>
+//         <div className="dispatch-image-container">
+//           <img src="/images/silhouette.jfif" alt="Profile" />
+//           <p className="dispatch-overlay-text">
+//             This is an inspirational quote for you to improve yourself and get
+//             better. Fight until the end.{" "}
+//             <p>
+//               Dig Deeper <MdOutlineReadMore size={25} />
+//             </p>
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default BlogPage;
---
+++ src/pages/ConnectionNote.jsx
@@ -0,0 +1,10 @@
+function ConnectionNote() {
+  return (
+    <div className="connectionNote-parent">
+      <div>Secure a Connection!</div>
+      <p>©2024 Joelinton, Inc. (Earl Morningstar). All Rights Reserved.</p>
+    </div>
+  );
+}
+
+export default ConnectionNote;
